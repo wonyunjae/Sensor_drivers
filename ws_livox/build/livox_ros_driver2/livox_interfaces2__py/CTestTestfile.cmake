0873b56fee477d7619783a4bbf0e76dbc2450f75
wynz <wyj5578@koreatech.ac.kr>
2025-03-04 15:21:59 +0900

    add MID 360 ros2 driver

---
+++ ws_livox/build/livox_ros_driver2/livox_interfaces2__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wynz/sensor_ws/src/Sensor_drivers/ws_livox/build/livox_ros_driver2/livox_interfaces2__py
+# Build directory: /home/wynz/sensor_ws/src/Sensor_drivers/ws_livox/build/livox_ros_driver2/livox_interfaces2__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
