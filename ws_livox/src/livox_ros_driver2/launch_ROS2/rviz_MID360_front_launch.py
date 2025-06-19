4a69ed8b5229a89fdb8bdb15b3a8416d1454d7f4
wynz <wyj5578@koreatech.ac.kr>
2025-06-19 13:01:48 +0900

    add mixed MID360 launch.py

--- ws_livox/src/livox_ros_driver2/launch_ROS2/rviz_MID360_front_launch.py
+++ ws_livox/src/livox_ros_driver2/launch_ROS2/rviz_MID360_front_launch.py
@@ -14,7 +14,7 @@ multi_topic   = 1    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
 publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
 output_type   = 0
-frame_id      = 'livox_frame'
+frame_id      = 'livox_frame_front'
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
 
